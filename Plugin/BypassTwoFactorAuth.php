c4fd687ec4d64d03b17ae1aa65c2bf71e1a3b1e8
Elaine Sutto <92882150+elaineosutto@users.noreply.github.com>
2021-10-20 22:30:21 -0300

    Add files via upload

---
+++ Plugin/BypassTwoFactorAuth.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace MarkShust\DisableTwoFactorAuth\Plugin;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\TwoFactorAuth\Model\TfaSession;
+
+/**
+ * Class BypassTwoFactorAuth
+ * @package MarkShust\DisableTwoFactorAuth\Plugin
+ */
+class BypassTwoFactorAuth
+{
+    const XML_PATH_CONFIG_ENABLE = 'twofactorauth/general/enable';
+
+    /** @var ScopeConfigInterface */
+    private $scopeConfig;
+
+    /**
+     * BypassTwoFactorAuth constructor.
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Enables the bypass of 2FA for admin access.
+     * This can be useful within development & integration environments.
+     *
+     * If 2FA is enabled, return the original result.
+     * If 2FA is disabled, always return true so all requests bypass 2FA.
+     *
+     * NOTE: Always keep 2FA enabled within production environments for security purposes.
+     *
+     * @param TfaSession $subject
+     * @param $result
+     * @return bool
+     */
+    public function afterIsGranted(
+        TfaSession $subject,
+        $result
+    ): bool {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_CONFIG_ENABLE)
+            ? $result
+            : true;
+    }
+}
